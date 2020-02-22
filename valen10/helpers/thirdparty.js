51716005e9489b09aa031427b76c78856a85d32d
Abhilash Gnan <abhilashgnan@gmail.com>
2020-02-22 09:07:31 +0100

    Add service worker to cache assets
    This change enables offline funcitonality of site. Hopefully the first page wont take too long to download all things to be cached before showing content to user

---
+++ valen10/helpers/thirdparty.js
@@ -0,0 +1,22 @@
+var THIRDPARTY_URLS = {
+  gifs: [
+    "https://media.giphy.com/media/XbsB79zhtQB9eUsBaU/giphy.gif",
+    "https://media.giphy.com/media/dG79iTcCx23Oo/giphy.gif",
+    "https://media.giphy.com/media/7k3ThwwMXnHCE/giphy.gif",
+    "https://media.giphy.com/media/3o7TKoWXm3okO1kgHC/giphy.gif",
+    "https://media.giphy.com/media/VNFJZ6mpsvfHO/giphy.gif",
+    "https://media.giphy.com/media/o0QIJqoObOK1G/giphy.gif",
+    "https://media.giphy.com/media/l4pTdcifPZLpDjL1e/giphy.gif",
+    "https://media.giphy.com/media/5dUllWbKVlaqmMTvHb/giphy.gif",
+    "https://media.giphy.com/media/oDzZcpDm6AYKI/giphy.gif",
+    "https://media.giphy.com/media/l0Hek8mXSBY772iMo/giphy.gif",
+    "https://media.giphy.com/media/xThtannt70SCAtcnSw/giphy.gif",
+    "https://media.giphy.com/media/26xBzKsEllzJUcdgI/giphy.gif",
+    "https://media.giphy.com/media/ZzquuaCvjrX8I/giphy.gif"
+  ],
+  dependencies: [
+    "https://code.jquery.com/jquery-3.4.1.min.js",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+  ]
+};
---
+++ valen10/sw.js
@@ -0,0 +1,32 @@
+importScripts("helpers/sw-polyfill.js");
+importScripts("helpers/thirdparty.js");
+
+var CACHED = [
+  "/",
+  "/index.html",
+  "/assets/favicon.png",
+  "styles.css",
+  "helpers/sw-helper.js",
+  "helpers/thirdparty.js",
+  "index.js"
+]
+  .concat(THIRDPARTY_URLS.dependencies)
+  .concat(THIRDPARTY_URLS.gifs);
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("valen10").then(function(cache) {
+      return cache.addAll(CACHED);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.response);
+    })
+  );
+});
