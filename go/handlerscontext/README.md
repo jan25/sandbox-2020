2cf69ab0f6550f5675f4e1e5c7819bb84e74b8bb
Abhilash Gnan <abhilashgnan@gmail.com>
2020-05-03 16:06:57 +0200

    setup root and child endpoints

---
+++ go/handlerscontext/README.md
@@ -0,0 +1,21 @@
+## Sample http server with logging, tracing and graceful shutdown
+
+`go run main.go` to start the server and `curl localhost:8080` in different terminal to see logs printed from server.
+
+`main.go` makes use of middlewares to inject and extract request info into global Context, which are avaible throught out request lifecycle.
+
+Server can be stopped with Interrupt(`ctrl+C`) or with other signals, so cleaning up resources before shutdown is useful.
+
+For e.g. duing k8s pods rolling updates, new pods are created to replace old ones, old ones receive `SIGTERM` and if not handled well open connections could fail. Instead server can wait until all active requests are finished and idle connections are closed. There might be additional support needed for this to work well - Readiness endpoint to fail when `SIGTERM` is received and Liveness need to work with presense of some /tmp file probe which can be cleaned up after all connections are closed.
+
+### Useful resources
+
+https://www.alexedwards.net/blog/making-and-using-middleware
+
+https://www.alexedwards.net/blog/a-recap-of-request-handling
+
+https://www.youtube.com/watch?v=LSzR0VEraWw&list=WL&index=15&t=0s
+
+https://medium.com/over-engineering/graceful-shutdown-with-go-http-servers-and-kubernetes-rolling-updates-6697e7db17cf
+
+https://golang.org/pkg/net/http/#Server.Shutdown
---
+++ go/handlerscontext/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"context"
+)
+
+type contextKey string
+
+const (
+	requestID     = "requestID"
+	tracingheader = "customtracingheader" // "traceid-parentid"
+	traceID       = "traceId"
+	parentID      = "parentId"
+	traceSep      = "-"
+)
+
+func setRequestContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set unique ID for current request
+		ctx := r.Context()
+		id := randID()
+		ctx = context.WithValue(ctx, contextKey(requestID), id)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func tracingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Parse trace headers
+		ctx := r.Context()
+		xID := r.Header.Get(tracingheader)
+		if xID == "" {
+			t := randID()
+			ctx = context.WithValue(ctx, contextKey(traceID), t)
+			ctx = context.WithValue(ctx, contextKey(parentID), "")
+		} else {
+			parts := strings.Split(xID, traceSep)
+			ctx = context.WithValue(ctx, contextKey(traceID), parts[0])
+			ctx = context.WithValue(ctx, contextKey(parentID), parts[1])
+		}
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Log request and trace info
+		ctx := r.Context()
+		path := r.RequestURI
+		printRequestLine(ctx, path)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+		id := ctx.Value(contextKey(requestID))
+		log.Printf("[%s] End of request. path: %s", id, path)
+	})
+}
+
+func printRequestLine(ctx context.Context, path string) {
+	id := ctx.Value(contextKey(requestID))
+	log.Printf("[%s] Start of request. path: %s", id, path)
+
+	t := ctx.Value(contextKey(traceID))
+	p := ctx.Value(contextKey(parentID))
+	log.Printf("traceID=%s; parentID:%s", t, p)
+}
+
+func createHandler(h http.Handler) http.Handler {
+	return setRequestContext(tracingMiddleware(loggingMiddleware(h)))
+}
+
+func main() {
+	rh := createHandler(http.HandlerFunc(rootHandler))
+	ch := createHandler(http.HandlerFunc(childHandler))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", rh)
+	mux.Handle("/child", ch)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	id := ctx.Value(contextKey(requestID))
+	w.Write([]byte("root: hello child!\n"))
+
+	req, err := http.NewRequest("GET", "http://localhost:8080/child", nil)
+	if err != nil {
+		log.Fatal(err)
+		w.Write([]byte("Error calling /child"))
+		return
+	}
+
+	tid := ctx.Value(contextKey(traceID))
+	header := fmt.Sprintf("%s%s%s", tid, traceSep, id)
+	req.Header.Set(tracingheader, header)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		w.Write([]byte("Error calling /child"))
+		return
+	}
+	defer resp.Body.Close()
+
+	msg, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		w.Write([]byte("Error reading response from /child"))
+		return
+	}
+	w.Write(msg)
+}
+
+func childHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("child: say hello to root\n"))
+}
+
+func randID() string {
+	return strconv.Itoa(rand.Int())
+}
